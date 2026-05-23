d57addd2b1e798e2b9d6eb9020e24932d5b99f8b
vandit004 <vandit.s004@gmail.com>
2026-05-24 00:24:21 +0530

    Configure PSQL database connection using JDBC

---
+++ README.md
@@ -0,0 +1,15 @@
+# Expense Tracker System
+
+A console-based Java application for tracking and managing daily expenses using PostgreSQL and JDBC.
+
+## Features
+- Add expenses
+- View all expenses
+- Delete expenses
+- Calculate total spending
+
+## Technologies Used
+- Java
+- PostgreSQL
+- JDBC
+- Git & GitHub
---
+++ src/DatabaseConnection.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5434/expense_tracker";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "Vandit2004@";
+
+    public static Connection connect() {
+
+        try {
+
+            Connection conn =
+                    DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database connected successfully!");
+
+            return conn;
+
+        } catch (Exception e) {
+
+            System.out.println("Connection failed!");
+
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}
