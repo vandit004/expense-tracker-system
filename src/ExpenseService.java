e1868fa2d6473f3fb5c1ea8d34969deea4434f00
vandit004 <vandit.s004@gmail.com>
2026-05-27 14:46:20 +0530

    Completed CRUD operations and added monthly expense summary feature

--- src/ExpenseService.java
+++ src/ExpenseService.java
@@ -1,21 +1,22 @@
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
- 
+
 public class ExpenseService {
 
     public void addExpense(Expense expense) {
 
-        String query =
-                "INSERT INTO expenses(title, amount, category, expense_date) VALUES (?, ?, ?, ?)";
+        String query
+                = "INSERT INTO expenses(title, amount, category, expense_date) VALUES (?, ?, ?, ?)";
 
         try {
 
             Connection conn = DatabaseConnection.connect();
 
-            PreparedStatement pstmt =
-                    conn.prepareStatement(query);
+            PreparedStatement pstmt
+                    = conn.prepareStatement(query);
 
             pstmt.setString(1, expense.getTitle());
             pstmt.setDouble(2, expense.getAmount());
@@ -35,21 +36,20 @@ public class ExpenseService {
         }
     }
 
-
     public void viewExpenses() {
 
-        try{
+        try {
 
             Connection connection = DatabaseConnection.connect();
 
             String query = "SELECT * FROM expenses";
-            
+
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while(resultSet.next()) {
+            while (resultSet.next()) {
 
-                int id  = resultSet.getInt("id");
+                int id = resultSet.getInt("id");
                 String title = resultSet.getString("title");
                 double amount = resultSet.getDouble("amount");
                 String category = resultSet.getString("category");
@@ -58,25 +58,22 @@ public class ExpenseService {
                 System.out.println("ID:" + id);
                 System.out.println("Title:" + title);
                 System.out.println("Amount:" + amount);
-                System.out.println("Category: "+ category);
-                System.out.println("Date:"+ expenseDate);
+                System.out.println("Category: " + category);
+                System.out.println("Date:" + expenseDate);
 
                 System.out.println("-------------------");
             }
 
-         
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-
     }
 
-    public void deleteExpense(int id ) {
+    public void deleteExpense(int id) {
 
         try {
-            
+
             Connection connection = DatabaseConnection.connect();
 
             String query = "Delete FROM expenses WHERE id = ?";
@@ -87,15 +84,84 @@ public class ExpenseService {
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            if(rowsAffected > 0){
+            if (rowsAffected > 0) {
                 System.out.println("Expense Deleted Sucessfully");
 
-            }else{
+            } else {
                 System.out.println("Expense ID not found");
             }
 
-                }catch (Exception e){
-            e.printStackTrace();    
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void updateExpenseAmount(int id, double newAmount) {
+
+        try {
+
+            Connection connection = DatabaseConnection.connect();
+
+            String query = "UPDATE expenses SET amount = ? WHERE id =?";
+
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement(query);
+
+            preparedStatement.setDouble(1, newAmount);
+
+            preparedStatement.setInt(2, id);
+
+            int rowsAffected
+                    = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+
+                System.out.println("Expense updated successfully!");
+
+            } else {
+
+                System.out.println("Expense ID not found.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMonthlyExpenseSummary(int month, int year) {
+
+        try {
+
+            Connection connection
+                    = DatabaseConnection.connect();
+
+            String query
+                    = "SELECT SUM(amount) FROM expenses "
+                    + "WHERE EXTRACT(MONTH FROM expense_date) = ? "
+                    + "AND EXTRACT(YEAR FROM expense_date) = ?";
+
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, month);
+
+            preparedStatement.setInt(2, year);
+
+            ResultSet resultSet
+                    = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+
+                double totalExpense
+                        = resultSet.getDouble(1);
+
+                System.out.println("Total Expense: " + totalExpense);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,128 @@
+
 import java.sql.Date;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-      ExpenseService service =
-            new ExpenseService();  
+        ExpenseService service
+                = new ExpenseService();
 
+        while (true) {
 
-    while(true){
+            System.out.println("\n===== Expense Tracker Menu =====");
+            System.out.println("1. ADD expenses");
+            System.out.println("2. VIEW expenses");
+            System.out.println("3. DELETE expenses");
+            System.out.println("4. UPDATE expenses");
+            System.out.println("5. Monthly expense Sumamry");
+            System.out.println("6. Exit     ");
 
-        System.out.println("\n===== Expense Tracker Menu =====");
-        System.out.println("1. ADD expenses");
-        System.out.println("2. VIEW expenses");
-        System.out.println("3. DELETE expenses");
-        System.out.println("4. Exit     ");
+            System.out.println("Enter your choice: ");
 
-        System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
 
-        int choice = sc.nextInt();
+            sc.nextLine();
 
-        sc.nextLine();
-
-        switch (choice) {
+            switch (choice) {
                 case 1:
 
-                        System.out.println("ADD Expense selected");
+                    System.out.println("ADD Expense selected");
 
-                        System.out.print("Enter category: ");
-                        String category = sc.nextLine();
+                    System.out.print("Enter category: ");
+                    String category = sc.nextLine();
 
-                        System.out.print("Enter amount: ");
-                        double amount = sc.nextDouble();
-                        sc.nextLine();
+                    System.out.print("Enter amount: ");
+                    double amount = sc.nextDouble();
+                    sc.nextLine();
 
-                        System.out.print("Enter description: ");
-                        String description = sc.nextLine();
+                    System.out.print("Enter description: ");
+                    String description = sc.nextLine();
 
-                        System.out.print("Enter date (YYYY-MM-DD): ");
-                        String dateInput = sc.nextLine();
+                    System.out.print("Enter date (YYYY-MM-DD): ");
+                    String dateInput = sc.nextLine();
 
-                        Date date = Date.valueOf(dateInput);
+                    Date date = Date.valueOf(dateInput);
 
-                        Expense expense =
-                        new Expense(
-                                category,
-                                 amount,
-                                 description,
-                                 date
-                                );
-                        
-                            
-                        break;
-                       
-        
+                    Expense expense
+                            = new Expense(
+                                    category,
+                                    amount,
+                                    description,
+                                    date
+                            );
+
+                    break;
+
                 case 2:
-                        service.viewExpenses();
-                        break;  
-                
+                    service.viewExpenses();
+                    break;
+
                 case 3:
-                        System.out.println("n====== Current Expenses =====");
+                    System.out.println("n====== Current Expenses =====");
 
-                        service.viewExpenses();
+                    service.viewExpenses();
 
-                         System.out.println("Enter Expense ID to delete");
+                    System.out.println("Enter Expense ID to delete");
 
-                        int id = sc.nextInt();
+                    int id = sc.nextInt();
 
-                        sc.nextLine();
-                        
-                        service.deleteExpense(id);
+                    sc.nextLine();
 
-                        
-                        break;
-        
-                case 4: 
-                        System.out.println("Exiting Application");
-                        System.exit(0);
-                
-                default: 
-                         System.out.println("Invalid choice");
-                         break;
+                    service.deleteExpense(id);
+
+                    break;
+
+                case 4:
+
+                    System.out.println("\n===== Current Expenses =====");
+
+                    service.viewExpenses();
+
+                    System.out.print("Enter Expense ID to update: ");
+
+                    int updateId = sc.nextInt();
+
+                    System.out.print("Enter new amount: ");
+
+                    double newAmount = sc.nextDouble();
+
+                    sc.nextLine();
+
+                    service.updateExpenseAmount(updateId, newAmount);
+
+                    break;
+
+                case 5:
+
+                    System.out.print("Enter month (1-12): ");
+
+                    int month = sc.nextInt();
+
+                    System.out.print("Enter year: ");
+
+                    int year = sc.nextInt();
+
+                    sc.nextLine();
+
+                    service.showMonthlyExpenseSummary(month, year);
+
+                    break;
+
+                case 6:
+                    System.out.println("Exiting Application");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
         }
-    }        
 
-    //service.addExpense(expense);
-
-    //service.viewExpenses();
-}
+        //service.addExpense(expense);
+        //service.viewExpenses();
+    }
 }
